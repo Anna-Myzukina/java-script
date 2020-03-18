0e42c681ffcf6cee53cf1604dc9598e692ba7966
Anna Muzykina <anna.muzykina83@gmail.com>
2020-03-18 12:18:16 +0200

    Create Staircase.js

---
+++ Staircase.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the staircase function below.
+function staircase(n) {
+var s = '';
+
+
+for (var i = 1; i <= n; i++){
+    s += ' '.repeat(n - i) + "#".repeat(i) + '\n';
+    
+}
+console.log(s);
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    staircase(n);
+}
